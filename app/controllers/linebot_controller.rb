1b43a387902347fc0a5155e76a7ca4876520de0c
ryshiba <race.lifes.a.race@gmail.com>
2020-06-24 16:35:08 +0900

    [add]ローカルでの動画返信

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,8 @@
-class LinebotController < ApplicationController
+require 'json'
+require 'uri'
+require 'net/http'
 
+class LinebotController < ApplicationController
   def callback
     body = request.body.read
     events = client.parse_events_from(body)
@@ -9,11 +12,23 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          uri = URI.parse('https://api.avgle.com/v1/videos/page=0?limit=100')
+          json = Net::HTTP.get(uri)
+          result = JSON.parse(json)
+          result['response']['videos'].each do |v|
+            if v['title'] == event.message['text'] || v['title'].include?(event.message['text']) || v['keyword'] == event.message['text'] || v['keyword'].include?(event.message['text'])
+              title = {
+                type: 'text',
+                text: v['title']
+              }
+              video = {
+                type: "video",
+                originalContentUrl: v['preview_video_url'],
+                previewImageUrl: v['preview_url']
+              }
+              client.reply_message(event['replyToken'], video)
+            end
+          end
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
           tf = Tempfile.open("content")
@@ -22,5 +37,16 @@ class LinebotController < ApplicationController
       end
     }
     "OK"
+  end
+end
+
+class Array
+  def index_select(obj = nil)
+    if obj.nil? && !block_given?
+      self.each
+    else
+      proc = obj.nil? ? ->(i){ yield self[i] } : ->(i){ self[i] == obj }
+      self.each_index.select(&proc)
+    end
   end
 end
