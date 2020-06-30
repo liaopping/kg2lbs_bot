5cb16c3271c99d5ee6896625f926ff83a75610e1
ryshiba <race.lifes.a.race@gmail.com>
2020-06-30 13:37:57 +0900

    [add]本日の視聴数トップ 5の返信機能

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,27 +12,47 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          uri = URI.parse('https://api.avgle.com/v1/videos/page=0?limit=100')
-          json = Net::HTTP.get(uri)
-          result = JSON.parse(json)
-          result['response']['videos'].each do |v|
-            if v['title'] == event.message['text'] || v['title'].include?(event.message['text']) || v['keyword'] == event.message['text'] || v['keyword'].include?(event.message['text'])
-              title = {
-                type: 'text',
-                text: v['title']
-              }
+          if event.message['text'] == '本日の視聴数トップ5'
+            uri = URI.parse('https://api.avgle.com/v1/videos/page=0?o=mv&t=t&limit=5')
+            json = Net::HTTP.get(uri)
+            result = JSON.parse(json)
+            @videos = []
+            result['response']['videos'].each do |v|
               video = {
                 type: "video",
-                originalContentUrl: v['preview_video_url'],
-                previewImageUrl: v['preview_url']
+                originalContentUrl: v.dig('preview_video_url'),
+                previewImageUrl: v.dig('preview_url')
               }
-              client.reply_message(event['replyToken'], video)
-            else
-              failure = {
-                type: 'text',
-                text: '検索ワードにヒットする動画が見つかりませんでした。'
-              }
-              client.reply_message(event['replyToken'], failure)
+              @videos << video
+            end
+              client.reply_message(event['replyToken'], @videos)
+          elsif event.message['カテゴリごとに探す']
+            #
+          elsif event.message['女優ごとに探す']
+            #
+          else
+            uri = URI.parse('https://api.avgle.com/v1/videos/page=0?limit=5')
+            json = Net::HTTP.get(uri)
+            result = JSON.parse(json)
+            result['response']['videos'].each do |v|
+              if v['title'] == event.message['text'] || v['title'].include?(event.message['text']) || v['keyword'] == event.message['text'] || v['keyword'].include?(event.message['text'])
+                title = {
+                  type: 'text',
+                  text: v['title']
+                }
+                video = {
+                  type: "video",
+                  originalContentUrl: v['preview_video_url'],
+                  previewImageUrl: v['preview_url']
+                }
+                client.reply_message(event['replyToken'], video)
+              else
+                failure = {
+                  type: 'text',
+                  text: '検索ワードにヒットする動画が見つかりませんでした。'
+                }
+                client.reply_message(event['replyToken'], failure)
+              end
             end
           end
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
@@ -43,16 +63,5 @@ class LinebotController < ApplicationController
       end
     }
     "OK"
-  end
-end
-
-class Array
-  def index_select(obj = nil)
-    if obj.nil? && !block_given?
-      self.each
-    else
-      proc = obj.nil? ? ->(i){ yield self[i] } : ->(i){ self[i] == obj }
-      self.each_index.select(&proc)
-    end
   end
 end
